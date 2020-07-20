cccb491df493cfb88cd1bf96dd57685a7a591c64
swapnil <swap81733@gmail.com>
2020-07-20 20:14:17 +0530

    add trailingSwipe,LeadingSwipe for edit and delete add SearchBar on storyboard use predicate for what type on search bar that display in tableview changes on loadData()

--- 19_COREDATA,PERSISTANT STORAGE/ViewController.swift
+++ 19_COREDATA,PERSISTANT STORAGE/ViewController.swift
@@ -74,14 +74,14 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     //read data from coreData Model
-    func loadData()
+    func loadData(with request:NSFetchRequest<Item> = Item.fetchRequest())
     {
         do
         {
             
             //Item is Entity
             
-            let request : NSFetchRequest<Item> = Item.fetchRequest()
+            //let request : NSFetchRequest<Item> = Item.fetchRequest() //we already add in funcation defination
             
             items =  try context.fetch(request)
             
@@ -112,8 +112,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
-        context.delete(items[indexPath.row])
-        items.remove(at:indexPath.row)
+       // context.delete(items[indexPath.row])
+        //items.remove(at:indexPath.row)
         
         items[indexPath.row].done = !items[indexPath.row].done
         
@@ -121,14 +121,95 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         tables.deselectRow(at: indexPath, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+
+        let deleteAction = UIContextualAction(style: .normal, title: "Delete") { (action, view, handler) in
+            
+
+            self.context.delete(self.items[indexPath.row])
+            self.items.remove(at: indexPath.row)
+            
+            self.saveData()
+            
+        }
+        
+        deleteAction.backgroundColor = .red
+        let config = UISwipeActionsConfiguration(actions: [deleteAction])
+        
+        return config
+    }
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        var textfiled = UITextField()
+        
+        let editAction = UIContextualAction(style: .normal,title: "Edit") { (action , view, handler) in
+            
+         
+            let alert = UIAlertController(title: "Edit data", message: "Write a new name", preferredStyle: .alert)
+        
+            let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+                self.items[indexPath.row].title = textfiled.text
+                
+                self.saveData()
+                
+                
+                    }
+            alert.addTextField { (alerttext) in
+                
+
+                alerttext.text = self.items[indexPath.row].title
+                
+                textfiled = alerttext
+                 
+            }
+            
+         
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+
+        editAction.backgroundColor = .green
+        let config = UISwipeActionsConfiguration(actions: [editAction])
+        
+        return config
+    }
 }
+
+//MARK: search Bar Method
 
 extension ViewController : UISearchBarDelegate
 {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-    }
+
+
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+
+
+        //Following Line which describe when we type in search box search text can be display
+       // let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+
+        let predicate = NSPredicate(format: "title BEGINSWITH[c] %@", searchBar.text!)
+
+
+        request.predicate = predicate
+
+
+        loadData(with: request)
+
+}
+    
+//    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//
+//        loadData()
+//    }
+    
+    
+   
+    
     
 }
 
